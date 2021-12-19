677397bc3cfe800afc99e1584d883d6434416786
David Eklund <deklund@gmail.com>
2021-12-18 19:09:02 -0500

    Day 6, part 1

---
+++ src/bin/day6.rs
@@ -0,0 +1,61 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+struct LanternFish {
+    counter: i32,
+}
+
+impl LanternFish {
+    fn simulate_day(&mut self) -> Option<LanternFish> {
+        if self.counter == 0 {
+            self.counter = 6;
+            return Some(LanternFish { counter: 8 });
+        } else {
+            self.counter -= 1;
+            return None;
+        }
+    }
+}
+
+struct LanternFishSet {
+    fish: Vec<LanternFish>,
+}
+
+impl LanternFishSet {
+    fn from_file(path: &str) -> LanternFishSet {
+        let line = BufReader::new(File::open(path).unwrap())
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap();
+        let fish: Vec<LanternFish> = line
+            .split(",")
+            .map(|count| count.parse::<i32>().unwrap())
+            .map(|counter| LanternFish{counter})
+            .collect();
+        LanternFishSet { fish }
+    }
+
+    fn simulate_day(&mut self) {
+        let mut newfish = Vec::new();
+        for fish in &mut self.fish {
+            match fish.simulate_day() {
+                Some(f) => newfish.push(f),
+                None => (),
+            }
+        }
+        self.fish.append(&mut newfish);
+    }
+
+    fn count(&self) -> usize {
+        self.fish.len()
+    }
+}
+
+fn main() {
+    let mut fishset = LanternFishSet::from_file("data/day6-input");
+    for day in 0..80 {
+        fishset.simulate_day();
+        println!("After {} days, there are {} fish", day+1, fishset.count());
+    }
+}
