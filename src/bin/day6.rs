d729f5f4c7c5918f776fade39894ae76a40a4fe7
David Eklund <deklund@gmail.com>
2021-12-18 19:21:56 -0500

    Day 6, part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -52,9 +52,39 @@ impl LanternFishSet {
     }
 }
 
+
+struct BigLanternFishSet {
+    fish: [i64; 9],
+}
+
+impl BigLanternFishSet {
+    fn from_file(path: &str) -> BigLanternFishSet {
+        let fishset = LanternFishSet::from_file(path);
+        let mut fisharray = [0; 9];
+        for f in fishset.fish {
+            fisharray[f.counter as usize] += 1;
+        }
+        BigLanternFishSet { fish: fisharray }
+    }
+
+    fn simulate_day(&mut self) {
+        let mut newarray = [0; 9];
+        for ind in 1..9 {
+            newarray[ind-1] = self.fish[ind];
+        }
+        newarray[6] += self.fish[0];
+        newarray[8] += self.fish[0];
+        self.fish = newarray;
+    }
+
+    fn count(&self) -> i64 {
+        self.fish.into_iter().sum::<i64>()
+    }
+}
+
 fn main() {
-    let mut fishset = LanternFishSet::from_file("data/day6-input");
-    for day in 0..80 {
+    let mut fishset = BigLanternFishSet::from_file("data/day6-input");
+    for day in 0..256 {
         fishset.simulate_day();
         println!("After {} days, there are {} fish", day+1, fishset.count());
     }
